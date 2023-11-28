2d704af9ae3241480f29f5d97ce664213e8f5566
Pinku1211 <pinkukumarsarker@gmail.com>
2023-11-29 02:59:25 +0600

    payment method implemented

--- index.js
+++ index.js
@@ -6,7 +6,8 @@ const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const jwt = require('jsonwebtoken')
 const cookieParser = require('cookie-parser')
 const morgan = require('morgan')
-require('dotenv').config();
+require('dotenv').config()
+const stripe = require('stripe')(process.env.STRIPE_SECRETE_KEY);
 const port = process.env.PORT || 5000;
 
 // middleware
@@ -172,23 +173,72 @@ async function run() {
           price: updatedMeal.price,
           rating: updatedMeal.rating,
           post_time: updatedMeal.post_time,
-          likes: parseInt(updatedMeal.likes),
+          likes: updatedMeal.likes,
           admin_name: updatedMeal.admin_name,
           admin_email: updatedMeal.admin_email,
         }
       }
-      const result = await jobCollection.updateOne(filter, job, options);
+      const result = await mealCollection.updateOne(filter, job, options);
       res.send(result)
     })
 
 
-    // user related api
+    // user related api----------------------------
+    app.get('/users', async (req, res) => {
+      const result = await userCollection.find().toArray()
+      res.send(result)
+    })
+
     app.get('/user/:email', async (req, res) => {
       const email = req.params.email
       const query = { email: email }
       const result = await userCollection.findOne(query);
       res.send(result)
     })
+
+    app.get('/users/:role', async(req, res) => {
+      const role = req.params.role
+      const query = {role : role}
+      const result = await userCollection.find(query).toArray()
+      res.send(result)
+    })
+
+    app.get('/user1/:id', async (req, res) => {
+      const id = req.params.id
+      console.log(id)
+      const query = { _id: new ObjectId(id) }
+      const result = await userCollection.findOne(query);
+      res.send(result)
+    })
+    app.put('/user1/:id', async(req, res) => {
+      const id = req.params.id
+      const filter = { _id: new ObjectId(id) };
+      const options = { upsert: true };
+      const makeAdmin = {
+        $set: {
+          "role" : "admin"
+        }
+      }
+      const result = await userCollection.updateOne(filter, makeAdmin, options)
+      res.send(result)
+    })
+
+    // stripe payment -----------------------------------
+    app.post('/create-payment-intent', async (req, res) => {
+      const {price} = req.body;
+      const amount = parseInt(price * 100);
+      console.log('amount', amount)
+      const paymentIntent = await stripe.paymentIntents.create({
+        amount: amount,
+        currency: 'usd',
+        payment_method_types: ['card']
+      });
+      res.send({
+        clientSecret: paymentIntent.client_secret
+      })
+    })
+    
+
 
 
     await client.db("admin").command({ ping: 1 });
